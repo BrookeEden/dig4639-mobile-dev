7eb289dd768456aef5d154c6d21bc484c95367e9
BrookeEden <BrookeEden@Brookes-MacBook-Pro.local>
2020-02-12 10:05:45 -0500

    Updates

---
+++ precache-manifest.d65c139566c755485dac938ce71b0aa9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58d6b5620b162e31b7608e0a63a8bd74",
+    "url": "/dig4639-mobile-dev/index.html"
+  },
+  {
+    "revision": "4ed7d9e6f4f0fc8f37fc",
+    "url": "/dig4639-mobile-dev/static/css/main.0ebde3ef.chunk.css"
+  },
+  {
+    "revision": "e02cba94be16e740280c",
+    "url": "/dig4639-mobile-dev/static/js/2.3ded2b1a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/dig4639-mobile-dev/static/js/2.3ded2b1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ed7d9e6f4f0fc8f37fc",
+    "url": "/dig4639-mobile-dev/static/js/main.389675a6.chunk.js"
+  },
+  {
+    "revision": "170169ccdc7dff24daa3",
+    "url": "/dig4639-mobile-dev/static/js/runtime-main.e7bb69b4.js"
+  }
+]);
