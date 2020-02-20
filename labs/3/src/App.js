6f11fbd755d13f51ba83cc55d8c6c4296ab89957
BrookeEden <BrookeEden@Brookes-MacBook-Pro.local>
2020-02-20 11:33:29 -0500

    lab 3

---
+++ labs/3/src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './App.css';
+import Card from './components/Card';
+
+function App() {
+  return (<Card content="This is a card!"></Card>);
+}
+export default App;
